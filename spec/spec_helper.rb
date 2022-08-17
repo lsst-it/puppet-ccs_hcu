6eae875fb853e143ea17307e95d3e076d1605984
Joshua Hoblitt <josh@hoblitt.com>
2022-08-17 09:32:12 -0700

    Update from voxpupuli modulesync_config 5.3.0

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,11 +15,3 @@ if File.exist?(File.join(__dir__, 'default_module_facts.yml'))
     add_custom_fact name.to_sym, value
   end
 end
-
-def fixtures_path
-  File.join(__dir__, 'fixtures')
-end
-
-RSpec.configure do |c|
-  c.hiera_config = File.join(fixtures_path, 'hiera.yaml')
-end
